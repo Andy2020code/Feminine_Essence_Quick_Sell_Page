148e24aa6b5c202fdfaed19128b2d01a9d0fb1b7
Andy2020code <andersonusa18@icloud.com>
2026-08-06 22:24:23 -0400

    delete

--- server/core/static/JS/partials/policy_page.js
+++
@@ -1,6 +0,0 @@
-function toggleDropdown(id, button) {
-    const dropdown = document.getElementById(id);
-    
-    dropdown.classList.toggle("active_dropdown");
-    button.classList.toggle("active_dropdown_icon_anim");
-}
