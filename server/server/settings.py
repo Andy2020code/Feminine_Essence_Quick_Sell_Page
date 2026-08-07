d01540b6b9ba1032cc60648dd092b8d7235ceee0
VFX Observatory <144275177+Andy2020code@users.noreply.github.com>
2026-08-07 00:08:10 -0400

    fix

--- server/server/settings.py
+++ server/server/settings.py
@@ -139,9 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [
-    BASE_DIR.parent / "static",
-]
+STATICFILES_DIRS = []
 STATIC_ROOT = BASE_DIR.parent / "staticfiles"
 
 MEDIA_URL = "/media/"
